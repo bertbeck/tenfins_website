402284effaaf2c18e1a168d2ce10aa6462c4b44f
dejanle <dejnik978@gmail.com>
2017-12-06 15:52:18 +0100

    added style and font-sizes for start pages

--- docs/App.js
+++ docs/App.js
@@ -22,28 +22,28 @@ class App extends Component {
 
               <NavbarNav className="ml-auto">
                 <NavItem>
-                  <NavLink className="nav-link" to="/">Home</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" id="tech" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/css">Tech</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" to="/css">Tech</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/components">Blog</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" to="/components">Blog</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/javascript">People</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" to="/javascript">People</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/javascript">Services</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" to="/javascript">Services</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/javascript">Contact us</NavLink>
+                  <NavLink className="nav-link js-scroll-trigger" to="/javascript">Contact us</NavLink>
                 </NavItem>
               </NavbarNav>
 
             </div>
           </Navbar>
-          <div className="start-page padding-style-page">
+          <div className="start-page">
             <div className="jumbotron">
               <div className="container text-center">
                 <div className="jumbotron-tenfins-section">
@@ -53,7 +53,7 @@ class App extends Component {
               </div>
             </div>
           </div>
-           <div className="technology padding-style-page">
+           <div id="tech" className="technology padding-style-page">
              <div className="container text-center">
                 <h3>Technology</h3> 
                 <h2>Mobile apps today can be developed with Native, Hybrid or Cross Platforms tools. 
@@ -61,25 +61,25 @@ class App extends Component {
                     Cross Platform tools use Native api's and are closer to Native performance
                     React Native and Google's new tool Flutter are Cross Platform tools.</h2>
                 <div className="row">
-                  <div className="col-md-3">
+                  <div className="col-md-4">
                     <div className="row">
                       <div className="col-md-12">
                         <h4>Google Flutter is new tech that is coming on strong and will challenge React Native.
                          We think Flutter has the potential to be a better solution.  
                          </h4>
-                         <h5>
+                         <h5 className="left-section">
                          "While it's still very early days, the team are onto something big, They have hit a sweet
                           spot of productivity and power, and have the momentum and backing to be successful.
                            The ability to write an app that runs on both iOS and Android,
                            with one code base, without compromise, using a highly productive 
                            language and set of libraries -- it's a compelling value proposition."
                          </h5>
-                         <p className="">- Tim Sneath, 17-Year Microsoft Vet now leading Google's Flutter Team
+                         <p className="author-style">- Tim Sneath, 17-Year Microsoft Vet now leading Google's Flutter Team
                          </p>
                       </div>
                     </div>
                     </div>
-                    <div className="col-md-9">
+                    <div className="col-md-8">
                       <div className="embed-responsive embed-responsive-16by9">
                         <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2F1BXg4wfB9pA%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1BXg4wfB9pA&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2F1BXg4wfB9pA%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
                       </div>
@@ -101,34 +101,39 @@ class App extends Component {
                <div className="container">
                 <div className="title-subtitle-section">
                   <h5>People</h5>
-                  <h2> Success comes from People and Technology you can rely on 
-                  We're based in Boulder, Colorado and can assist locally or remotely
-                  We've got expertise in React Native, Flutter and Backends in C#, Dart and Java
+                  <h2> Success comes from People and Technology you can rely on <br />
+                  We're based in Boulder, Colorado and can assist locally or remotely <br />
+                  We've got expertise in React Native, Flutter and Backends in C#, Dart and Java <br />
                   .</h2>
                 </div> 
-              <div className="row">
-                    <div className="col-md-9">
-                          <div className="embed-responsive embed-responsive-16by9">
-                             <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
-                          </div> 
-                    </div>
-                    <div className="col-md-3">
-                      <h2>React Native</h2>
-                      <p>Scaling to 80 Million Users </p>
-                      <p>React Native is making inroads into mainstream Mobile cross platform development. </p>
-                    </div>
-                  
-                    <div className="col-md-3">
-                      <h2>Flutter vs React Native</h2>
-                      <p>Scaling to 80 Million Users </p>
-                      <p>Mobile developers compare Flutter and React Native </p>
-                    </div>
-                     <div className="col-md-9">
-                          <div className="embed-responsive embed-responsive-16by9">
-                             <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
-                      </div> 
-                    </div>
-                 </div>
+                <div className=" padding-style-page-video">
+                  <div className="row">
+                      <div className="col-md-7">
+                            <div className="embed-responsive embed-responsive-16by9">
+                               <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
+                            </div> 
+                      </div>
+                      <div className="col-md-5">
+                        <h2>React Native</h2>
+                        <p className="video-text-style">Scaling to 80 Million Users </p>
+                        <p>React Native is making inroads into <br /> mainstream Mobile cross platform <br /> development. </p>
+                      </div>
+                  </div>
+                <div className=" padding-style-page-video">
+                  <div className="row">  
+                      <div className="col-md-5">
+                        <h2>Flutter vs React Native</h2>
+                        <p className="video-text-style">Scaling to 80 Million Users </p>
+                        <p>Mobile developers compare Flutter and React Native </p>
+                      </div>
+                       <div className="col-md-7">
+                            <div className="embed-responsive embed-responsive-16by9">
+                               <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FabSNo2P9mMM%3Fwmode%3Dtransparent%26feature%3Doembed&amp;wmode=transparent&amp;url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DabSNo2P9mMM&amp;image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FabSNo2P9mMM%2Fhqdefault.jpg&amp;key=0ae70bf4c11f11e09f134040d3dc5c07&amp;type=text%2Fhtml&amp;schema=youtube" scrolling="no" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
+                        </div> 
+                      </div>
+                   </div>
+                </div>
+                </div>
               </div>
               </div>
               <div className="services-section padding-style-page">
